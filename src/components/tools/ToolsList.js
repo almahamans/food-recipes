b29273bf1a5d2e7a239b77e8a9b2a69e5d11674a
Almaha <mahaw999@gmail.com>
2024-02-20 23:02:29 +0300

    update the code

--- src/components/tools/ToolsList.js
+++ src/components/tools/ToolsList.js
@@ -1,9 +1,11 @@
+import '../../style/listStyle.css'
+
 function ToolsList({tool_name, desc}){
     return(
-        <section>
-            <li>{tool_name}</li>
-            <p>{desc}</p>
-        </section>
+        <section className='ml-9 mb-6 mt-4 w-[80%]'>
+            <li className='li'>{tool_name}</li>
+            <p>{desc}</p> 
+        </section> 
     )
 }
 export default ToolsList;
