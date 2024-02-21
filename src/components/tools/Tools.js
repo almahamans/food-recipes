dc9dc8419bff9720bf5e4322c246ac5d97751b46
Almaha <mahaw999@gmail.com>
2024-02-21 13:39:43 +0300

    adding image

--- src/components/tools/Tools.js
+++ src/components/tools/Tools.js
@@ -1,13 +1,14 @@
 import ToolsList from "./ToolsList";
 import toolsItems from './ToolsFile.json';
 import '../../style/fonts.css';
+import chefTools from './chef-tools-names.jpg'
 
 function Tools({name}){
     return(
         <section className="">
             <h1 className=" textCenter text-[20px] font-sans">{name} Tools:</h1>
             {/* <p className="ml-5">Here's list of {name} tools that needed in cooking:</p> */}
-            <img src="chef_tools.jpg"  height={`300em`} width={`300em`}/>
+            <img src={chefTools} alt='chef_tools' height={`70%`} width={`80%`} className="mx-auto my-7" />
             <ul>
                {toolsItems.map((item)=>
                 <ToolsList key={item.id} tool_name={item.name} desc={item.describe} />               
