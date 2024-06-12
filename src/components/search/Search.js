a5e2baec152f88f99c4fe0a686a5de60b6b0a427
Almaha <mahaw999@gmail.com>
2024-06-12 12:23:52 +0300

    update

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -27,17 +27,17 @@ function Search() {
   }, [search])
 
 
-  const clearSearch = () => {
-    setSearch('')
-  }
+  // const clearSearch = () => {
+  //   setSearch('')
+  // }
   
   return(
   <div className="bg-white">
     <form  className='flex flex-col justify-center items-center flex-grow relative '>
       <label htmlFor="food" className='mt-9 mb-4 font-lobester text-red-800 text-2xl md:text-md'>Type a food name or one ingredient:</label>
       <section className='flex flex-row'>    
-      <input type="text" name="food" id ="food" autoComplete='no' placeholder='ex rice...' value={search} onChange={(e) => setSearch(e.target.value)} className='rounded-md mb-3 mr-5 border-2 p-2 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 w-64' />
-      <button onClick={clearSearch} className='rounded-md bg-red-800 px-2 h-9 mt-1 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'>Clear</button>
+      <input type="text" name="food" id ="food" autoComplete='no' spellCheck='true' placeholder='ex rice...' value={search} onChange={(e) => setSearch(e.target.value)} className='rounded-md mb-3 mr-5 border-2 p-2 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 w-64' />
+      {/* <button onClick={clearSearch} className='rounded-md bg-red-800 px-2 h-9 mt-1 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'>Clear</button> */}
       </section>
     </form> 
     {isLoading ? (
